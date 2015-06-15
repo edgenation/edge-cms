95709b62091231b2c94e1935cd4fa1592d681d3a
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-06-15 16:48:56 +0100

    Removing method-override

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,5 @@
 var Q = require("q"),
     mongoose = require("mongoose"),
-    methodOverride = require("method-override"),
     express = require("express"),
     compression = require("compression"),
     bodyParser = require("body-parser"),
@@ -13,30 +12,28 @@ var API = function () {
     this.app = null;
 };
 
-API.prototype.createApp = function () {
+API.prototype.createApp = function (options) {
     this.app = express();
     this.app.use(compression());
     //this.app.use(bodyParser.urlencoded({extended: true}));
     //this.app.use(bodyParser.json());
     this.app.use(responseTime());
     this.app.use(helmet());
-    this.app.use(methodOverride());
     this.app.use("/api", require("./route/index"));
 
-    this.app.set("port", process.env.PORT || 4000);
-    this.app.set("host", process.env.HOST || "0.0.0.0");
+    this.app.set("port", options.port || 4000);
+    this.app.set("host", options.host || "0.0.0.0");
 
     return this.app;
 };
 
 API.prototype.useApp = function (app) {
     this.app = app;
-    this.app.use(methodOverride());
     this.app.use("/api", require("./route/index"));
 };
 
-API.prototype.connectDB = function () {
-    return Q.ninvoke(mongoose, "connect", "mongodb://localhost/edge-cms-prototype");
+API.prototype.connectDB = function (path) {
+    return Q.ninvoke(mongoose, "connect", path);
 };
 
 API.prototype.disconnectDB = function () {
