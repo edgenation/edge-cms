4b9b608343ace59473428493146bd874f23c158d
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-06-15 18:00:00 +0100

    Refactoring index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-var api = require("./api/app");
-var site = require("./site/app");
-var errorHandler = require("./site/errorHandler");
-var router = require("./site/router");
-var config = require("./site/config");
+var _ = require("lodash");
+var requireDir = require("require-dir");
 
-module.exports = {
-    api: api,
-    site: site,
-    errorHandler: errorHandler,
-    router: router,
-    config: config
-};
+module.exports = _.assign({}, requireDir("./site"), requireDir("./api"));
