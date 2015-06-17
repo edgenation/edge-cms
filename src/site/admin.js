67192b13e8ebf24afb64b12bad5d6fec666cc4f3
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-06-17 15:07:20 +0100

    Adding basic admin view

--- src/site/admin.js
+++ src/site/admin.js
@@ -4,7 +4,7 @@ var adminRouter = express.Router();
 
 adminRouter.get("*", function (req, res, next) {
     // Server the admin route!
-    return res.send("admin");
+    return res.render("templates/admin");
 });
 
 adminRouter.middleware = function (options) {
