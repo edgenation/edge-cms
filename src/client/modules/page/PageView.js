2f82746c3a66d8713d69d52a916e5e2283d43a8e
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-06-18 17:50:11 +0100

    Experimenting with collection and model loading

--- src/client/modules/page/PageView.js
+++ src/client/modules/page/PageView.js
@@ -1,10 +1,59 @@
+var Backbone = require("backbone");
+
 var Marionette = require("backbone.marionette");
 
 var template = require("./page.jade");
+var childTemplate = require("./pageItem.jade");
 
-var PageView = Marionette.ItemView.extend({
+
+
+var Page = Backbone.Model.extend({
+    urlRoot: "/api/page",
+
+    initialize: function() {
+    },
+
+    parse: function(response, options) {
+        console.log(options);
+        //var data = response.data.attributes;
+        //data.id = response.data.id;
+
+        var data = response.attributes;
+        data.id = response.id;
+        return data;
+    }
+});
+
+var Pages = Backbone.Collection.extend({
+    model: Page,
+    url: "/api/page",
+
+    parse: function(response) {
+        return response.data;
+    }
+});
+
+
+
+var PageItemView = Marionette.ItemView.extend({
+    template: childTemplate,
+    className: "pageItem",
+    tagName: "li"
+});
+
+var PageView = Marionette.CompositeView.extend({
     template: template,
-    className: "page"
+    childView: PageItemView,
+    childViewContainer: ".pages",
+    className: "page",
+
+    initialize: function () {
+        this.collection = new Pages();
+    },
+
+    onShow: function () {
+        this.collection.fetch();
+    }
 });
 
 module.exports = PageView;
