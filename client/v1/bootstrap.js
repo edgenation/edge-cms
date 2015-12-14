8a02fbdce394013f3216351a0c5cefc1585bae06
Chris Sheppard <chris.sheppard@thebioagency.com>
2015-12-14 17:42:05 +0000

    Removing old stuff

--- client/v1/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import cms from "./cms";
-
-angular.element(document).ready(function () {
-    console.log("Bootstrap Ready!", cms.name);
-    angular.bootstrap(document.querySelector("[data-cms-app]"), [
-        cms.name
-    ], {strictDi: true});
-});
